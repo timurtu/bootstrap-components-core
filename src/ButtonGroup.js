90194f10da60ec89a4ad97b6b4c10c7d9704c2db
Timur Uzel <timurt.uzel@gmail.com>
2017-03-13 19:16:02 -0400

    bugfix

--- src/ButtonGroup.js
+++ src/ButtonGroup.js
@@ -5,12 +5,15 @@
 import React from 'react'
 import Button from './Button'
 
-const ButtonGroup = ({ children }) => (
+const ButtonGroup = ({ children, size }) => (
 
   <div className="btn-group" role="group">
     {[].concat(children).map((button, i) =>
 
-      <Button key={i}>
+      <Button
+        key={i}
+        size
+      >
 
         {button.props.children}
       </Button>
