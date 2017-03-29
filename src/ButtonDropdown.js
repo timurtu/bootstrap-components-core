202821c4b1be6181fecb3f29d133be5c4eead8ab
Timur Uzel <timurt.uzel@gmail.com>
2017-03-28 20:59:57 -0400

    bugfix

--- src/ButtonDropdown.js
+++ src/ButtonDropdown.js
@@ -3,26 +3,16 @@
  */
 
 import React from 'react'
+import ButtonGroup from './ButtonGroup'
 
-const ButtonDropdown = () => (
-  <div className="btn-group">
-    <button
-      type="button"
-      className="btn btn-default dropdown-toggle"
-      data-toggle="dropdown"
-      aria-haspopup="true"
-      aria-expanded="false"
-    >
-      Action <span className="caret" />
-    </button>
-    <ul className="dropdown-menu">
-      <li><a href="#">Action</a></li>
-      <li><a href="#">Another action</a></li>
-      <li><a href="#">Something else here</a></li>
-      <li role="separator" className="divider" />
-      <li><a href="#">Separated link</a></li>
-    </ul>
-  </div>
+const ButtonDropdown = ({ title, type, children }) => (
+  <ButtonGroup
+    dropdown
+    title={title}
+    type={type}
+  >
+    {children}
+  </ButtonGroup>
 )
 
 export default ButtonDropdown
