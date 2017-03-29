f90f183357b28b25849c22d5c30b85057389f77b
Timur Uzel <timurt.uzel@gmail.com>
2017-03-29 02:04:07 -0400

    bugfix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import Alert from './Alert.js';
 import Button from './Button.js';
-import ButtonDropdown from './ButtonDropdown.js';
 import ButtonGroup from './ButtonGroup.js';
 import Container from './Container.js';
 import Dropdown from './Dropdown.js';
@@ -10,7 +9,6 @@ import Icon from './Icon.js';
 export {
   Alert,
   Button,
-  ButtonDropdown,
   ButtonGroup,
   Container,
   Dropdown,
