530d7a4eb838077601d0b5546da55010146392b1
Timur Uzel <timurt.uzel@gmail.com>
2017-05-01 20:16:46 -0400

    bugfix

--- src/Container.js
+++ src/Container.js
@@ -5,7 +5,10 @@
 import React from 'react'
 
 const Container = ({ fluid, ...props }) => (
-  <div className={`container${props.fluid ? '-fluid' : ''}`}>
+  <div
+    {...props}
+    className={`container${props.fluid ? '-fluid' : ''}`}
+  >
     {props.children}
   </div>
 )
